5402bb4e9435975b740aa79ead62d0c1a0ae3616
garrotini <cgarrote92@gmail.com>
2026-03-13 23:26:09 +0000

    parsing 'map->width' fixed

--- src/parsing.c
+++ src/parsing.c
@@ -33,34 +33,55 @@ static int	count_lines(char *filename)
 	return (close(fd), count);
 }
 
+int	calc_width(t_map *map)
+{
+    int n;
+    int i;
+    int len;
+
+    n = 0;
+    i = 0;
+    while (map->grid[i])
+    {
+	len = ft_strlen(map->grid[i]);
+	if (len > n)
+	    n = len;
+	i++;
+    }
+    return (n);
+}
+
 static t_map	*fill_map(t_map *map, char *filename)
 {
-	int		fd;
-	char	*line;
-	size_t	i;
-	size_t len;
+    int		fd;
+    char	*line;
+    size_t	i;
+    size_t len;
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (perror("Error:\n"), NULL);
-	i = 0;
-	line = NULL;
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		len = ft_strlen(line);
-		while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r'))  //mudo ou deixo assim?
-		{
-			line[len - 1] = '\0';
-			len--;
-		}
-		map->grid[i++] = line;
-	}
-	map->width = ft_strlen(map->grid[0]);
-	map->grid[i] = NULL;
-	return (close(fd), map);
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+	    return (perror("Error:\n"), NULL);
+    i = 0;
+    line = NULL;
+    while (1)
+    {
+	    line = get_next_line(fd);
+	    if (!line)
+		    break ;
+	    len = ft_strlen(line);
+	    while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r'))  //mudo ou deixo assim?
+	    {
+		    line[len - 1] = '\0';
+		    len--;
+	    }
+	    map->grid[i++] = line;
+    }
+    
+    // cg: replaced this with the following calc_width()
+    // map->width = ft_strlen(map->grid[0]);
+    map->width = calc_width(map);
+    map->grid[i] = NULL;
+    return (close(fd), map);
 }
 
 t_map   *parse_map(char *filename)
